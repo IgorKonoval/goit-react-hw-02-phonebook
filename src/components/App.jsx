7a7d7b7c0fd66c7445387d6d9d71bf28dff2cb78
Igor Konoval <kip_y@sfa.org.ua>
2023-11-06 23:14:29 +0200

    add step 1 & 5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+const Button = ({ onAdd }) => {
+  return <button onClick={onAdd}>Add contact</button>;
 };
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleChange = event => {
+    this.setState({ name: event.target.value });
+  };
+
+  addContact = () => {
+    const newContact = {
+      id: nanoid(),
+      name: this.state.name,
+    };
+    if (
+      this.state.contacts.find(
+        contact => contact.name.toLowerCase() === this.state.name.toLowerCase()
+      )
+    ) {
+      return alert(`${this.state.name} is already in contacts!`);
+    }
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  render() {
+    const { contacts, name } = this.state;
+    return (
+      <section>
+        <h2>Phonebook</h2>
+        <div>
+          <h3>Name</h3>
+          <form>
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.handleChange}
+              required
+            />
+          </form>
+          <Button onAdd={this.addContact} />
+        </div>
+        <h2>Contacts</h2>
+        <ul>
+          {contacts.map(contact => (
+            <li key={contact.id}>{contact.name}</li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+}
