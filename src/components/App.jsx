6cceca7a9f33a0f57a3d31b6d0842b7151517cae
Igor Konoval <kip_y@sfa.org.ua>
2023-11-26 20:23:07 +0200

    add HW

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,80 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
-
-const Button = ({ onAdd }) => {
-  return <button onClick={onAdd}>Add contact</button>;
-};
+import ContactForm from './Form/Form';
+import { Container, Section, Title, SectionTitle, Message } from './App.styled';
+import { ContactList } from './ContactList/ContactList';
+import Filter from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [],
-    name: '',
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
-  handleChange = event => {
-    this.setState({ name: event.target.value });
-  };
-
-  addContact = () => {
-    const newContact = {
-      id: nanoid(),
-      name: this.state.name,
-    };
+  addContact = ({ name, number }) => {
+    const contact = { id: nanoid(), name, number };
     if (
       this.state.contacts.find(
-        contact => contact.name.toLowerCase() === this.state.name.toLowerCase()
+        contact => contact.name.toLocaleLowerCase() === name.toLocaleLowerCase()
       )
     ) {
-      return alert(`${this.state.name} is already in contacts!`);
+      return alert(`${name} is already in contacts!`);
     }
     this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
+      contacts: [contact, ...prevState.contacts],
     }));
   };
-
+  filterContacts = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+  getFilteredContacts = () => {
+    const { filter, contacts } = this.state;
+    const filterLowCase = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(filterLowCase)
+    );
+  };
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
   render() {
-    const { contacts, name } = this.state;
+    const filteredContacts = this.getFilteredContacts();
+
     return (
-      <section>
-        <h2>Phonebook</h2>
-        <div>
-          <h3>Name</h3>
-          <form>
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleChange}
-              required
-            />
-          </form>
-          <Button onAdd={this.addContact} />
-        </div>
-        <h2>Contacts</h2>
-        <ul>
-          {contacts.map(contact => (
-            <li key={contact.id}>{contact.name}</li>
-          ))}
-        </ul>
-      </section>
+      <Container>
+        <Title>Phonebook</Title>
+        <Section>
+          <SectionTitle>Add contact</SectionTitle>
+          <ContactForm onSubmit={this.addContact} />
+        </Section>
+        <Section>
+          <SectionTitle>Contacts</SectionTitle>
+          {this.state.contacts.length !== 0 ? (
+            <>
+              <Filter
+                value={this.state.filter}
+                onChange={this.filterContacts}
+              />
+              <ContactList
+                contacts={filteredContacts}
+                onDeleteBut={this.deleteContact}
+              />
+            </>
+          ) : (
+            <Message>
+              There are no contacts in your phonebook. Please add your first
+              contact!
+            </Message>
+          )}
+        </Section>
+      </Container>
     );
   }
 }
